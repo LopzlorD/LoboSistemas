c95ff4302fb4ec693367f1286bfb0816bb0e7368
branyerbjr <branyerbjr@gmail.com>
2024-07-25 11:18:29 -0500

    Nueva version

--- src/api/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api', // Cambia esto a la URL de tu API
-    timeout: 1000,
-    headers: { 'Content-Type': 'application/json' },
-});
-
-export default axiosInstance;
